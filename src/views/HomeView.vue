58014bdb7c15109675d5531ef83457816d7966cd
shrfrn <shrfrn@gmail.com>
2023-02-11 14:09:47 +0200

    Remove content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <h1>Home</h1>
+    </main>
 </template>
