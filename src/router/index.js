ff69d2c7f87756602c9fe89415f38e24c3555505
shrfrn <shrfrn@gmail.com>
2023-02-11 14:14:53 +0200

    Clean up router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../pages/HomeView.vue'
 
 const routerOptions = {
     history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -13,7 +13,7 @@ const routerOptions = {
             // route level code-splitting
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue'),
+            component: () => import('../pages/AboutView.vue'),
         },
     ],
 }
